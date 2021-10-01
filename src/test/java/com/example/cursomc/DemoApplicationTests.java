d9732916cb82d5f0ff6bfe738c99ab947a2046b1
Jorge Wellison <jorge.wellison@phoebus.local>
2021-10-01 18:30:42 -0300

    Consegui fazer o h2 pegar

--- src/test/java/com/example/cursomc/DemoApplicationTests.java
+++ src/test/java/com/example/cursomc/DemoApplicationTests.java
@@ -1,13 +1,9 @@
 package com.example.cursomc;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
