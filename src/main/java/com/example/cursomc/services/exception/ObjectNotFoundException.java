a55e9d438f0174bbcec7205dcb6b4d5cb6144b89
Jorge Wellison <jorge.wellison@phoebus.local>
2021-10-02 08:38:02 -0300

    Estado e Cidade

---
+++ src/main/java/com/example/cursomc/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package com.example.cursomc.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+
+    public ObjectNotFoundException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
